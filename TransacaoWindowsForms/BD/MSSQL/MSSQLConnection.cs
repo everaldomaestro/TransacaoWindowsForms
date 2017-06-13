b60d0bacd4122737b7926f8a04a59568e6c12eb1
everaldo.almeida <everaldo.almeida@gmail.com>
2017-06-13 13:48:39 -0300

    .

--- TransacaoWindowsForms/BD/MSSQL/MSSQLConnection.cs
+++ TransacaoWindowsForms/BD/MSSQL/MSSQLConnection.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace BD.MSSQL
 {
     class MSSQLConnection
     {
+        //Data Source = SERVER,PORT; 
+        //Initial Catalog = BD;
+        //User Id = USER; 
+        //Password=PASS;";
+        //String de conexão com o banco
+        private string SQLStringCon = @"Data Source = x,x; Initial Catalog = x; User Id = x; Password = x;";
+        private static SqlConnection SQLCon;
+        private static SqlTransaction SQLTran;
+        private static SqlCommand SQLCmd;
+
+        public void TransacaoSQL(string[] cmds)
+        {
+            using (SQLCon = new SqlConnection())
+            {
+                SQLCon.ConnectionString = SQLStringCon;
+                SQLCon.Open();
+                SQLTran = SQLCon.BeginTransaction();
+
+                try
+                {
+                    foreach (string cmdTxt in cmds)
+                    {
+                        SQLCmd = SQLCon.CreateCommand();
+                        SQLCmd.CommandText = cmdTxt;
+                        SQLCmd.Transaction = SQLTran;
+                        SQLCmd.ExecuteNonQuery();
+                    }
+                    SQLTran.Commit();
+                }
+                catch (SqlException ex)
+                {
+                    SQLTran.Rollback();
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    SQLCon.Close();
+                }
+            }
+        }
     }
 }
--- TransacaoWindowsForms/BD/MySQL/MySQLConnection.cs
+++ TransacaoWindowsForms/BD/MySQL/MySQLConnection.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace BD.MySQL
 {
     class MySQLConnection
     {
+        private string MySQLStringCon = "server=x;port=x;database=x;uid=x;pwd=x;";
+        private static MySqlConnection MySQLCon;
+        private static MySqlTransaction MySQLTran;
+        private static MySqlCommand MySQLCmd;
+
+        public void TransacaoMySQL(string[] cmds)
+        {
+            using (MySQLCon = new MySqlConnection())
+            {
+                MySQLCon.ConnectionString = MySQLStringCon;
+                MySQLCon.Open();
+                MySQLTran = MySQLCon.BeginTransaction();
+
+                try
+                {
+                    foreach (string cmdTxt in cmds)
+                    {
+                        MySQLCmd = MySQLCon.CreateCommand();
+                        MySQLCmd.CommandText = cmdTxt;
+                        MySQLCmd.Transaction = MySQLTran;
+                        MySQLCmd.ExecuteNonQuery();
+                    }
+
+                    MySQLTran.Commit();
+                }
+                catch (MySqlException ex)
+                {
+                    MySQLTran.Rollback();
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    MySQLCon.Close();
+                }
+            }
+        }
     }
 }
